4c24746e843f1cc98d918a79412587a78c41fc0f
Voltron7 <aquastar.minsk@gmail.com>
2022-04-16 07:31:39 +0300

    4. Переопределение [#174890 #289403]

--- src/test/java/ru/job4j/inheritance/JSONReportTest.java
+++ src/test/java/ru/job4j/inheritance/JSONReportTest.java
@@ -9,11 +9,24 @@ public class JSONReportTest {
     public void whenTestGenerateMethod() {
         String ln = System.lineSeparator();
         String expected = "{" + ln
-                + "\t\"name\" : \"name\"," + ln
-                + "\t\"body\" : \"body\"" + ln
+                + "\t\"name\" : \"Report's name\"," + ln
+                + "\t\"body\" : \"Report's body\"" + ln
                 + "}";
-        String name = "name";
-        String body = "body";
+        String name = "Report's name";
+        String body = "Report's body";
+        String result = new JSONReport().generate(name, body);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenNameIsDavidBodyIsNameSong() {
+        String ln = System.lineSeparator();
+        String expected = "{" + ln
+                + "\t\"name\" : \"David Gilmour\"," + ln
+                + "\t\"body\" : \"Shine On You Crazy Diamond\"" + ln
+                + "}";
+        String name = "David Gilmour";
+        String body = "Shine On You Crazy Diamond";
         String result = new JSONReport().generate(name, body);
         assertEquals(expected, result);
     }
