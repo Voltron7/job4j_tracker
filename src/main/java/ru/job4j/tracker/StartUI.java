3726efb822fdaf13492d97cba41d5369910abc04
Voltron7 <aquastar.minsk@gmail.com>
2022-04-16 10:23:11 +0300

    6. Date. Отображение даты. [#310017 #289542]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import java.time.format.DateTimeFormatter;
 
-public class StartUI extends Item{
+public class StartUI extends Item {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         Item item = new Item();
