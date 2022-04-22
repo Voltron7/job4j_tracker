481b385274463d1de5b1151ac5ed077ba44cfc71
Voltron7 <aquastar.minsk@gmail.com>
2022-04-22 10:28:58 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #291999]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -37,11 +37,10 @@ public class StartUITest {
         Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()),
-                "deleted item"
+                String.valueOf(item.getId())
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertNull(null, deleted);
+        assertNull(deleted);
     }
 }
