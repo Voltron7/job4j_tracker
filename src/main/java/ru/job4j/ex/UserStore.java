5b3f1d5ad8ea72e854dd1cd5959ee8b848e67362
Voltron7 <aquastar.minsk@gmail.com>
2022-04-27 13:31:14 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,42 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User user = null;
+        for (User find : users) {
+            if (login.equals(find.getUsername())) {
+                user = find;
+                break;
+            }
+        }
+        if (user == null) {
+            throw new UserNotFoundException("User has not found");
+        }
+        return user;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (
+                UserInvalidException ui) {
+            ui.printStackTrace();
+        } catch (
+                UserNotFoundException un) {
+            un.printStackTrace();
+        }
+    }
+}
