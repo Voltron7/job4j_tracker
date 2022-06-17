63c7bd3cb5fa7066d72e5c399934e6f758ab9bdc
Voltron7 <aquastar.minsk@gmail.com>
2022-06-17 18:26:56 +0300

    0. Stream API улучшения [#58167]

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -6,17 +6,21 @@ public class Student {
     private int score;
     private String surname;
 
-    public Student(int score, String surname) {
-        this.score = score;
-        this.surname = surname;
-    }
-
     public int getScore() {
         return score;
     }
 
     public String getSurname() {
         return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Student(int score, String surname) {
+        this.score = score;
+        this.surname = surname;
     }
 
     @Override
