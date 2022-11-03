78e6b0ed86b586ad4a7bd3e1472317bb95967560
Voltron7 <aquastar.minsk@gmail.com>
2022-11-03 15:04:45 +0300

    2. Трекер SQL [#1734 #357291]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -30,7 +30,7 @@ public class StartUI {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output,
                 new ConsoleInput()
