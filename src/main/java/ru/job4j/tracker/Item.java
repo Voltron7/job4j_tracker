a98c2a4a4730e027f59690dfbefe5232a6faf72e
Voltron7 <aquastar.minsk@gmail.com>
2023-09-25 14:01:05 +0300

    2. @Setter, @Getter и другие [#504889]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,32 +3,28 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item implements Comparable<Item> {
+    @Setter
+    @EqualsAndHashCode.Include
     private int id;
+    @NonNull
+    @Setter
+    @EqualsAndHashCode.Include
     private String name;
     private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
     private static final DateTimeFormatter FORMATTER
             = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
-    public Item() {
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public Item(int id, String name, LocalDateTime created) {
-        this.id = id;
-        this.name = name;
-        this.created = created;
     }
 
     @Override
